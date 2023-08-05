4f6f19af7c2fde133c51603f9dc8db0be4c8c2c4
Abaranasia <rankirlian@gmail.com>
2023-08-06 01:39:09 +0200

    Update server to add basic REST endpoints weith feedback

--- models/server.js
+++ models/server.js
@@ -21,7 +21,38 @@ class Server {
 
     routes() {
         this.app.get('/api', (req, res) => {
-            res.send("You're getting from the server")
+            res.json({
+                response: "get was ok",
+                msg: "You're getting from the server",
+            })
+        });
+
+        this.app.post('/api', (req, res) => {
+            res.json({
+                response: "post was ok",
+                msg: "You're posting to the server",
+            })
+        });
+
+        this.app.put('/api', (req, res) => {
+            res.json({
+                response: "put was ok",
+                msg: "You're putting to the server",
+            })
+        });
+
+        this.app.patch('/api', (req, res) => {
+            res.json({
+                response: "patch was ok",
+                msg: "You're patching to the server",
+            })
+        });
+
+        this.app.delete('/api', (req, res) => {
+            res.json({
+                response: "delete was ok",
+                msg: "You're deleting from the server",
+            })
         });
     }
 
